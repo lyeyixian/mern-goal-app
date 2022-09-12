95c9c59c36bf6315580b091cce6202725bb0d7c8
Lye Yi Xian <lyeyx999@hotmail.com>
2022-09-12 22:06:14 +0800

    Fix finding user again after auth middleware.

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -40,16 +40,14 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error('Goal not found!')
   }
 
-  const user = await User.findById(req.user.id)
-
   // Check user
-  if (!user) {
+  if (!req.user) {
     res.status(401)
     throw new Error('User not found!')
   }
 
   // Make sure logged in user matches the goal user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(403)
     throw new Error('User is not authorized!')
   }
@@ -70,16 +68,14 @@ const deleteGoal = asyncHandler(async (req, res) => {
     throw new Error('Goal not found!')
   }
 
-  const user = await User.findById(req.user.id)
-
   // Check user
-  if (!user) {
+  if (!req.user) {
     res.status(401)
     throw new Error('User not found!')
   }
 
   // Make sure logged in user matches the goal user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(403)
     throw new Error('User is not authorized!')
   }
