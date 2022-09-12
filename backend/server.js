19ee2bee10185bb5234e5b98a12c94156efea67d
Lye Yi Xian <lyeyx999@hotmail.com>
2022-09-12 16:15:55 +0800

    Connect mongodb. Create Goal model. Create CRUD APIs.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 8080
 const { errorHandler } = require('./middleware/errorMiddleware')
 
+const connectDB = require('./config/db')
+connectDB()
+
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
