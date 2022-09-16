11868e3a07fc847768f9d81699b4bca27b0f129e
Lye Yi Xian <lyeyx999@hotmail.com>
2022-09-16 16:53:01 +0800

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8000
 const { errorHandler } = require('./middleware/errorMiddleware')
 
 const connectDB = require('./config/db')
@@ -12,6 +13,14 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+  })
+}
+
 app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Express server listening on port ${PORT}`))
